8ce0929b181b96c392251778b232cbf84d98fc97
Jerry <jvr572@Gmail.com>
2019-03-02 16:06:04 -0500

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,11 @@ def first_challenge
     }
   }
 
-  #your code here
+  contact.each do |person, data|
+    data.each do |att, v|
+      v.delete_if { |e| e == "strawberry" }
 
 
-  #remember to return your newly altered contacts hash!
+
   contacts
 end
-
